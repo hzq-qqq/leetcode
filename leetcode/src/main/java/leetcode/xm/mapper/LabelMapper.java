7341404049568e54be41b33fccdb15593cb9cb31
hzq-qqq <72299739+hzq-qqq@users.noreply.github.com>
2021-02-28 10:50:33 +0800

    Revert "第一次"
    This reverts commit a418d302abcec27bc42fb4f29061fdf2bea042dd.

--- leetcode/src/main/java/leetcode/xm/mapper/LabelMapper.java
+++
@@ -1,9 +0,0 @@
-package leetcode.xm.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import leetcode.xm.pojo.Label;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface LabelMapper extends BaseMapper<Label> {
-}
--- leetcode/src/main/java/leetcode/xm/mapper/ProblemDescMapper.java
+++
@@ -1,9 +0,0 @@
-package leetcode.xm.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import leetcode.xm.pojo.Problemdesc;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ProblemDescMapper extends BaseMapper<Problemdesc> {
-}
