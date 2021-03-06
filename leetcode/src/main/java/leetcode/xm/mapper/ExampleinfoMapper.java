082fc4f1a128c400c94c8d6c55d1fcf4d4a63ae3
hzq-qqq <72299739+hzq-qqq@users.noreply.github.com>
2021-03-06 19:31:29 +0800

    提交错误

--- leetcode/src/main/java/leetcode/xm/mapper/ExampleinfoMapper.java
+++
@@ -1,9 +0,0 @@
-package leetcode.xm.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import leetcode.xm.pojo.Exampleinfo;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ExampleinfoMapper extends BaseMapper<Exampleinfo>{
-}
